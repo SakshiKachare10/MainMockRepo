b42f79e41d1fd1a17150090fdb212aa097e3c64a
Sakshikachare10 <sakshikachare23@gmail.com>
2024-12-28 14:52:27 +0530

    login page is created

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+let form = document.getElementById("form")
+
+form.addEventListener("submit" , function(){
+    event.preventDefault()
+    let email = form.email.value;
+    let password = form.password.value;
+    console.log(email,password)
+
+    if(email=="admin@empher.com" && password=="empher@123"){
+      alert("Logged in as Admin.")
+      window.location.href="admin.html"
+    }else {
+        alert("Something went wrong.Try Again Later.")
+    }
+    // window.location.href="admin.html"
+    
+    if(email=="user@empher.com" && password=="user@123"){
+        alert("Logged in as Admin.")
+        window.location.href="books.html"
+      }else {
+          alert("Something went wrong.Try Again Later.")
+      }
+})
